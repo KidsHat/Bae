ba2aa591af1287c6a4c1ebfb71b5103a503fc025
KidsHat <89117861+KidsHat@users.noreply.github.com>
2021-08-18 19:46:40 +0700

    upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Cục Cưng ơiiiiii',
+    introDesc: `Anh có cái này muốn hỏi em nèèèèèè`,
+    btnIntro: 'Nhấn vào là yeu Huy',
+    title: 'Em có muốn làm vợ anh hong dọ😙',
+    desc: 'Trả lời thật lòng nhah !! ',
+    btnYes: 'Có Chồng iuuuu',
+    btnNo: 'Làm vợ cc',
+    question:'Thật hong dọ😙 ',
+    btnReply: 'Xác Nhận',
+    reply: 'Dạ đúng rồi chồnggggggggggg',
+    mess: 'hjhj yêu vợ',
+    messDesc: 'yêu emmmmmmmmmm.♥♥♥♥',
+    btnAccept: 'Ok anhh iuu',
+    messLink: 'https://www.facebook.com/HuyGGwp' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
